1fb228b19c1e126e2cd0dff235c547318e8ae7f3
ShovaSarker <shovasarker01@gmail.com>
2022-03-01 07:45:30 +0600

    getSearchText,getFetchUrl,fetchPhoneInfo function is completed for fetching phones info from the api

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
-console.log('hello')
+const getSearchText = (searchFieldId) => {
+  const searchField = document.getElementById(searchFieldId)
+  const searchText = searchField.value
+  return searchText ? searchText : -1
+}
+
+//getting Dynamic url from parameters
+const getfetchUrl = (searchId, search = false) => {
+  const baseUrl = 'https://openapi.programming-hero.com/api/phone'
+  return (fetchUrl = `${baseUrl}${search ? 's?search=' : '/'}${searchId}`)
+}
+
+const fetchPhoneInfo = async (fetchUrl) => {
+  try {
+    const res = await fetch(fetchUrl)
+    const result = await res.json()
+    return result.data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const displayPhoneInfo = (phones) => {}
+
+const searchPhone = async () => {
+  const searchText = getSearchText('search-field')
+  if (searchText === -1) {
+    //display error message
+    return
+  }
+  const fetchUrl = getfetchUrl(searchText, true)
+  const data = await fetchPhoneInfo(fetchUrl)
+  displayPhoneInfo(data)
+}
