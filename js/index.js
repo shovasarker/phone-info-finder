365d79c9b0891ace6ae0e434c4a8da390b1693ae
ShovaSarker <shovasarker01@gmail.com>
2022-03-01 14:28:24 +0600

    Svg icons changes into font-awesome icons

--- js/index.js
+++ js/index.js
@@ -2,6 +2,16 @@
 Later to display all the results */
 let phonesInfo = []
 
+//Constant Object for font-awesome icons Class
+const iconsMap = {
+  radio: 'fa-solid fa-tower-broadcast',
+  usb: 'fa-brands fa-usb',
+  wlan: 'fa-solid fa-wifi',
+  bluetooth: 'fa-brands fa-bluetooth',
+  gps: 'fa-solid fa-globe',
+  nfc: 'fa-solid fa-satellite-dish',
+}
+
 // Function for getting the searchText from the inpuit field
 const getSearchText = (searchFieldId) => {
   const searchField = document.getElementById(searchFieldId)
@@ -72,6 +82,7 @@ const displayPhoneDetails = (phone) => {
     ? Object.entries(others).forEach(([key, value]) => {
         const h3 = document.createElement('h3')
         h3.innerHTML = `
+          <i class="mr-1.5 ${iconsMap[key.toLowerCase()]}"></i>
             ${key}: <span class='font-bold'>${
           value ? value : NO_INFO_FOUND
         }</span>
@@ -123,10 +134,7 @@ const displayPhoneInfo = (phones, showAll = false) => {
             <h2 class='card__title'>${phone_name}</h2>
             <h4 class='card__brand-name'>${brand}</h4>
             <button class='custom-btn' onclick="searchPhoneDetails('${slug}')">
-            View Details
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-3 h-3 inline-block ml-1 mb-0.5 fill-white ">
-            <path d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z"/>
-            </svg>
+            View Details<i class="ml-1.5 fa-solid fa-arrow-right"></i>
             </button>
         `
     cardContainer.appendChild(div)
